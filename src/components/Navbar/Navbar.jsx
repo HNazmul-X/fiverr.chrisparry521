74de1c2970f1b4fe2d525a3d0c500a1f325f9b21
H. Nazmul Hassan <Nazmul.w3@gmail.com>
2021-11-16 21:27:31 +0600

    responsive completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [isDropdownShow, setIsDropdownShow] = useState(false);
+    const [isMblSearchbarShow, setIsMblSearchbarShow] = useState(false);
 
     useEffect(() => {
         if (isDropdownShow) {
@@ -19,29 +20,40 @@ const Navbar = () => {
             <div className="container-fluid">
                 <div className="navbar-item-container">
                     <div className="navbar-brand">Logo</div>
-                    <div className="navbar-searchBar">
+                    <div className={`${isMblSearchbarShow ? "active" : ""} navbar-searchBar`}>
                         <div className="search-bar">
                             <span className="search-bar__icon">
-                                <InlineIcon icon="carbon:search" style={{ fontSize: "25px" }} />
+                                {isMblSearchbarShow ? (
+                                    <InlineIcon onClick={() => setIsMblSearchbarShow(!isMblSearchbarShow)} icon="la:times" style={{ fontSize: "25px" }} />
+                                ) : (
+                                    <InlineIcon icon="carbon:search" style={{ fontSize: "25px" }} />
+                                )}
                             </span>
                             <input type="text" placeholder="Search Your Items" />
                         </div>
                     </div>
                     <div className="navbar-items">
+                        <li onClick={() => setIsMblSearchbarShow(!isMblSearchbarShow)} className="d-block d-md-none">
+                            <Link to="/">
+                                <InlineIcon icon="carbon:search" style={{ fontSize: "25px" }} />
+                            </Link>
+                        </li>
                         <li>
                             <Link to="/">
                                 <InlineIcon style={{ fontSize: "25px" }} icon="clarity:sun-line" />
                             </Link>
                         </li>
-                        <li>
+                        <li className="d-none d-md-block">
                             <Link to="/">FAQ</Link>
                         </li>
                         <li>
-                            <div style={{fontWeight:500,cursor:"pointer"}}>
-                                <div onClick={(e) => {
-                                    e.stopPropagation()
-                                    setIsDropdownShow(!isDropdownShow);
-                                }} className={`focus-dropdown  ${isDropdownShow ? "active" : ""}`}>
+                            <div style={{ fontWeight: 500, cursor: "pointer" }}>
+                                <div
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        setIsDropdownShow(!isDropdownShow);
+                                    }}
+                                    className={`focus-dropdown  ${isDropdownShow ? "active" : ""}`}>
                                     <div className="focus-dropdown__label">Connect wallet</div>
                                     <div className="focus-dropdown__items">
                                         <div className="focus-dropdown__items-wrapper">
